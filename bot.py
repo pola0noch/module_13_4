5403fd0d119b7a13a695416be7da8157311d0da6
pola0noch <klayriddell@gmail.com>
2024-11-07 13:36:12 +0300

    init

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = '8125982558:AAGmyXr1-seEDL5AzYm-Au8tuy39-oRmTIw'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
